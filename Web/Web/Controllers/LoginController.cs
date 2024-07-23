f259e602c280a8d604e0fff3daf7cf61fa2cf147
Yose <dwiyogaseptia@gmail.com>
2024-07-23 19:45:27 +0700

    Changes

---
+++ Web/Web/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class LoginController : BaseController
+    {
+
+        //private readonly IHttpClientFactory _httpClientFactory;
+        public LoginController(IApiService apiService) : base(apiService)
+        {
+        }
+        public IActionResult Login()
+        {
+            ClearLoginSession();
+            return View();
+        }
+        public async Task<IActionResult> CheckLogin(ms_user _obj)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ClearLoginSession();
+                    var userData = await _apiService.GetUserLogin(_obj.user_name, _obj.password);
+                    ms_user user = JsonSerializer.Deserialize<ms_user>(userData);
+                    if (!string.IsNullOrEmpty(user.user_name))
+                    {
+                        HttpContext.Session.SetString("username", user.user_name); //set session
+                        await CurrentUser(user);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return BadRequest("Email / Password is wrong");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error");
+                }
+            }
+            return RedirectToAction(nameof(Login));
+        }
+
+
+        public IActionResult Logout()
+        {
+            ClearLoginSession();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ Web/Web/Services/IApiService.cs
@@ -0,0 +1,86 @@
+﻿
+//{
+//    
+//}
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Web.Models;
+
+public interface IApiService
+{
+    Task<string> GetApiResponseAsync();
+    Task<string> GetUserLogin(string usn, string pass);
+    Task<string> GetAllLocation();
+    Task<string> GetOneLocation(string id);
+    Task<string> SaveTransaction(TransactionModel obj);
+    Task<string> GetListTransaction();
+}
+
+public class ApiService : IApiService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<ApiService> _logger;
+
+    public ApiService(HttpClient httpClient, ILogger<ApiService> logger)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+
+    public async Task<string> GetApiResponseAsync()
+    {
+        var response = await _httpClient.GetAsync("https://localhost:7246/Master/GetAllUser");
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return responseContent;
+    }
+    public async Task<string> GetUserLogin(string usn, string pass)
+    {
+        var response = await _httpClient.GetAsync("https://localhost:7246/Master/GetByUsername/" + usn + "/" + pass);
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return responseContent;
+    }    
+    public async Task<string> GetAllLocation()
+    {
+        var response = await _httpClient.GetAsync("https://localhost:7246/Master/GetAllLocation");
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return responseContent;
+    }    
+    public async Task<string> GetOneLocation(string id)
+    {
+        var response = await _httpClient.GetAsync("https://localhost:7246/Master/GetOneLocation/" + id);
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return responseContent;
+    }
+    public async Task<string> SaveTransaction(TransactionModel obj)
+    {
+        obj.created_on = DateTime.Now;
+        obj.last_updated_on = DateTime.Now;
+        string json = JsonConvert.SerializeObject(obj);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync("https://localhost:7025/Transaction/Save", content);
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return responseContent;
+    }
+    public async Task<string> GetListTransaction()
+    {
+        var response = await _httpClient.GetAsync("https://localhost:7025/Transaction/GetAllTransaction");
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return responseContent;
+    }
+}
